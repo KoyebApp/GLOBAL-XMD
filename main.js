df50d128a60c93ba3cd183a89418cc7ed047ee46
Koyeb App <koyebapp@gmail.com>
2025-07-12 02:30:40 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1636,39 +1636,33 @@ module.exports = qasim = async (qasim, m, msg, store, groupCache) => {
 			}
 			break
 			case 'ping': case 'botstatus': case 'statusbot': {
-				const used = process.memoryUsage()
-				const cpus = os.cpus().map(cpu => {
-					cpu.total = Object.keys(cpu.times).reduce((last, type) => last + cpu.times[type], 0)
-					return cpu
-				})
-				const cpu = cpus.reduce((last, cpu, _, { length }) => {
-					last.total += cpu.total
-					last.speed += cpu.speed / length
-					last.times.user += cpu.times.user
-					last.times.nice += cpu.times.nice
-					last.times.sys += cpu.times.sys
-					last.times.idle += cpu.times.idle
-					last.times.irq += cpu.times.irq
-					return last
-				}, {
-					speed: 0,
-					total: 0,
-					times: {
-						user: 0,
-						nice: 0,
-						sys: 0,
-						idle: 0,
-						irq: 0
-					}
-				})
-				let timestamp = speed()
-				let latensi = speed() - timestamp
-				neww = performance.now()
-				oldd = performance.now()
-				respon = `Response Speed ${latensi.toFixed(4)} _Second_ \n ${oldd - neww} _miliseconds_\n\nRuntime : ${runtime(process.uptime())}\n\n💻 Info Server\nRAM: ${formatp(os.totalmem() - os.freemem())} / ${formatp(os.totalmem())}\n\n_NodeJS Memory Usaage_\n${Object.keys(used).map((key, _, arr) => `${key.padEnd(Math.max(...arr.map(v=>v.length)),' ')}: ${formatp(used[key])}`).join('\n')}\n\n${cpus[0] ? `_Total CPU Usage_\n${cpus[0].model.trim()} (${cpu.speed} MHZ)\n${Object.keys(cpu.times).map(type => `- *${(type + '*').padEnd(6)}: ${(100 * cpu.times[type] / cpu.total).toFixed(2)}%`).join('\n')}\n_CPU Core(s) Usage (${cpus.length} Core CPU)_\n${cpus.map((cpu, i) => `${i + 1}. ${cpu.model.trim()} (${cpu.speed} MHZ)\n${Object.keys(cpu.times).map(type => `- *${(type + '*').padEnd(6)}: ${(100 * cpu.times[type] / cpu.total).toFixed(2)}%`).join('\n')}`).join('\n\n')}` : ''}`.trim()
-				m.reply(respon)
-			}
-			break
+
+  const start = now();
+
+  // Send placeholder message
+  let pingMsg = await qasim.sendMessage(m.chat, { text: '🏓 Pinging...' }, { quoted: m });
+
+  // Wait for neofetch just to simulate delay (optional)
+  exec('neofetch --stdout', async () => {
+    const latency = (now() - start).toFixed(2);
+
+    // Edit original message
+    await qasim.relayMessage(
+      m.chat,
+      {
+        protocolMessage: {
+          key: pingMsg.key,
+          type: 14,
+          editedMessage: {
+            conversation: `*GlobalBotInc 💘 running ping:* *${latency} ms*`
+          }
+        }
+      },
+      {}
+    );
+  });
+}
+  break
 			case 'speedtest': case 'speed': {
 				m.reply('Testing Speed...')
 				let cp = require('child_process')
